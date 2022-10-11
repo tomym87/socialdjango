b264cf15c65130a2720ff5a9ff3d1f7c746983a7
Ubuntu <ubuntu@ip-172-31-30-37.us-east-2.compute.internal>
2022-10-11 20:47:58 +0000

    subiendo desde server

--- social_project/social_project/settings.py
+++ social_project/social_project/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-63v!%qe2_=02yqv(tcexvha$tek^g19zlz7p8vx7bf2%g_%20u'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
@@ -120,8 +120,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR, "static/")
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
